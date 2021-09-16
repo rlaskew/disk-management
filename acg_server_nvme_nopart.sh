cdee8b991fbcf57cf17772c85c4d74520ab05e17
root <root@3d7e445f021c.mylabserver.com>
2021-09-16 14:28:20 +0000

    Initial commit

---
+++ acg_server_nvme_nopart.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# ACG cloud server with Amazon Linux 2 .. they add nvme1n1 or 2 drive for your
+
+## from
+## https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ebs-using-volumes.html
+## https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/device_naming.html
+
+# Step 1: become root
+# sudo -i
+
+# find disk name
+# lsblk -f
+
+# format disk .. in this case no partitioning -- NAME in tihs case is nvme1n1
+# mkfs.xfs /dev/${NAME}
+
+# make director where you want new device to be mounted to
+# mkdir /data
+
+# manually mount first
+# mount /dev/${NAME} /data
+
+# make backup copy of fstab
+# cp /etc/fstab /etc/fstab.orig
+
+# find new device UUID blkid or lsblk
+# lsblk -o +UUID 
+
+# add entry to fstab, obvi your UUID will be different
+# UUID=aebf131c-6957-451e-8d34-ec978d9581ae  /data  xfs  defaults,nofail  0  2
+
+# sanity check fstab is ok
+# umount /data
+# mount -a
+
+# if you can't fix fstab errors
+# mv /etc/fstab.orig /etc/fstab
